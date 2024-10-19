4c828e2c57ebc20c9e6f2c32c72c88a3a6c8590a
SergiDemo <demovedion@gmail.com>
2024-10-19 17:55:55 +0500

    removed empty spaces what are we living for

--- spec/models/joke_spec.rb
+++ spec/models/joke_spec.rb
@@ -1,15 +1,15 @@
-  # frozen_string_literal: true
+# frozen_string_literal: true
 
-  RSpec.describe Joke do
-    describe 'associations' do
-      it { is_expected.to belong_to(:user) }
-    end
+RSpec.describe Joke do
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
 
-    describe 'validations' do
-      it 'is valid with valid attributes' do
-        user = User.create!(username: 'testuser')
-        joke = described_class.new(body: 'This is a joke', user: user)
-        expect(joke).to be_valid
-      end
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      user = User.create!(username: 'testuser')
+      joke = described_class.new(body: 'This is a joke', user: user)
+      expect(joke).to be_valid
     end
   end
+end
