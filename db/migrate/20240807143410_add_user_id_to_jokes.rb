ec8cf48ef44904ccbdc6d865c3cac8103e37200f
nikolaipolevoi <nikolaiapolevoi@gmail.com>
2024-10-19 13:45:02 +0400

    Fix ur shit

--- db/migrate/20240807143410_add_user_id_to_jokes.rb
+++ db/migrate/20240807143410_add_user_id_to_jokes.rb
@@ -1,8 +1,7 @@
 class AddUserIdToJokes < ActiveRecord::Migration[7.1]
   def up
     add_reference :jokes, :user, foreign_key: true
-    default_user = User.first
-    Joke.update_all(user_id: default_user.id)
+    Joke.update_all(user_id: User.first)
   end
 
   def down
