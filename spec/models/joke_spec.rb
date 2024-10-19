1ef7dbdba9dd3630fcb280f34a7bad010ded8f2a
SergiDemo <demovedion@gmail.com>
2024-10-19 17:16:20 +0500

    added end in spec/models/joke_spec.rb and added config/environments/development.rb in allcops exclude

--- spec/models/joke_spec.rb
+++ spec/models/joke_spec.rb
@@ -1,13 +1,16 @@
+# frozen_string_literal: true
+
 RSpec.describe Joke do
   RSpec.describe Joke do
     describe 'associations' do
-      it { should belong_to(:user) }
+      it { is_expected.to belong_to(:user) }
+    end
     end
 
     describe 'validations' do
       it 'is valid with valid attributes' do
         user = User.create!(username: 'testuser')
-        joke = Joke.new(body: 'This is a joke', user: user)
+        joke = described_class.new(body: 'This is a joke', user: user)
         expect(joke).to be_valid
       end
     end
